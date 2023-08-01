f39405cba275bf7a89ad3fdb7df505aab3710065
Garrett Tordo <76716199+PrinceFluffHK@users.noreply.github.com>
2023-08-01 14:42:21 -0400

    seeders, jsonSchema validations, relationMappings

--- server/src/db/Seeder.js
+++ server/src/db/Seeder.js
@@ -1,9 +1,20 @@
 /* eslint-disable no-console */
 import { connection } from "../boot.js"
+import ReviewSeeder from "./seeders/ReviewSeeder.js"
+import SiteSeeder from "./seeders/SiteSeeder.js"
+import UserSeeder from "./seeders/UserSeeder.js"
 
 class Seeder {
   static async seed() {
     // include individual seed commands here
+    console.log("seeding sites")
+    await SiteSeeder.seed()
+
+    console.log("seeding users")
+    await UserSeeder.seed()
+
+    console.log("seeding reviews")
+    await ReviewSeeder.seed()
 
     console.log("Done!")
     await connection.destroy()
--- server/src/models/Site.js
+++ server/src/models/Site.js
@@ -3,7 +3,46 @@ const Model = require("./Model.js")
 class Site extends Model {
     static get tableName() {
         return "sites"
-    } 
+    }
+
+    static get jsonSchema() {
+        return {
+            type: "object",
+            required: ["name", "address", "description"],
+            properties: {
+                name: { type: "string" },
+                address:{ type: "string" },
+                description:{ type: "string" }
+            }
+        }
+    }
+
+    static get relationMappings() {
+        const { User, Review } = require("./index.js")
+        return {
+            users: {
+                relation: Model.ManyToManyRelation,
+                modelClass: User,
+                join: {
+                    from: "sites.id",
+                    through: {
+                        from: "reviews.siteId",
+                        to: "reviews.userId"
+                    },
+                    to: "users.id"
+                }
+            },
+
+            reviews: {
+                relation: Model.HasManyRelation,
+                modelClass: Review,
+                join: {
+                    from: "sites.id",
+                    to: "reviews.siteId"
+                }
+            }
+        }
+    }
 }
 
 module.exports = Site
