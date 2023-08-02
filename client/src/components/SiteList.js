cb6c528fa1c2a89660e3c48a4cae2a9428c45c83
Garrett Tordo <76716199+PrinceFluffHK@users.noreply.github.com>
2023-08-02 14:40:07 -0400

    adhered to Kerrin's infernal bidding

--- client/src/components/SiteList.js
+++ client/src/components/SiteList.js
@@ -2,48 +2,45 @@ import React, { useEffect, useState } from "react";
 import SiteTile from "./SiteTile";
 
 const SiteList = (props) => {
-  const [siteList, setSiteList] = useState([]);
+    const [siteList, setSiteList] = useState([]);
 
-  const getSites = async () => {
-    try {
-      const response = await fetch("/api/v1/sites");
-      if (!response.ok) {
-        const errorMessage = `${response.status} (${response.statusText})`;
-        const error = new Error(errorMessage);
-        throw error;
-      }
-      const responseBody = await response.json();
-      setSiteList(responseBody.siteList);
-    } catch (error) {
-      console.error(`Error in Fetch: ${error.message}`);
-    }
-  };
+    const getSites = async () => {
+        try {
+            const response = await fetch("/api/v1/sites");
+            if (!response.ok) {
+                const errorMessage = `${response.status} (${response.statusText})`;
+                const error = new Error(errorMessage);
+                throw error;
+            }
+            const responseBody = await response.json();
+            setSiteList(responseBody.siteList);
+        } catch (error) {
+            console.error(`Error in Fetch: ${error.message}`);
+        }
+    };
 
-  useEffect(() => {
-    getSites();
-  }, []);
+    useEffect(() => {
+        getSites();
+    }, []);
 
-  const sitesToRender = siteList.map((site) => {
+    const sitesToRender = siteList.map((site) => {
+        return (
+            <SiteTile
+                name={site.name}
+                address={site.address}
+                description={site.description}
+                setting={site.setting}
+                minimumAge={site.minimumAge}
+            />
+        );
+    });
+
     return (
-      <li key={site.id}>
-        <SiteTile
-          key={site.id}
-          name={site.name}
-          address={site.address}
-          description={site.description}
-          setting={site.setting}
-          minimumAge={site.minimumAge}
-        />
-      </li>
+        <>
+            <h1>Welcome to Boston!</h1>
+            <ul>{sitesToRender}</ul>
+        </>
     );
-  });
-
-  return (
-    <>
-      <h1>Welcome to Boston!</h1>
-      <ul>{sitesToRender}</ul>
-    </>
-  );
 };
 
 export default SiteList;
--- server/src/models/Site.js
+++ server/src/models/Site.js
@@ -1,24 +1,24 @@
-const Model = require("./Model.js")
+const Model = require("./Model.js");
 
 class Site extends Model {
-  static get tableName() {
-    return "sites"
-  }
-
-  static get jsonSchema() {
-    return{
-      type: "object",
-      required: ["name", "address", "description"],
-      properties: {
-        name: { type: "string", minLength: 1 },
-        address: { type: "string", minLength: 1 },
-        description: { type: "string", minLength: 1},
-        setting: { type: "string" },
-        minimumAge: { type: ["string", "integer"] },
-        imageUrl: { type: "string" }
-      }
+    static get tableName() {
+        return "sites";
     }
-  }
+
+    static get jsonSchema() {
+        return {
+            type: "object",
+            required: ["name", "address", "description"],
+            properties: {
+                name: { type: "string" },
+                address: { type: "string" },
+                description: { type: "string" },
+                setting: { type: "string" },
+                minimumAge: { type: ["string", "integer"] },
+                imageUrl: { type: "string" },
+            },
+        };
+    }
 }
 
-module.exports = Site
+module.exports = Site;
